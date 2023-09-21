c3f01b84eff336e6c78ac246840e57c7cee6eb54
tahirakdeniz <muhammedtahirakdeniz@gmail.com>
2023-09-21 10:00:21 +0300

    [Improved] leetcode/169

--- leetcode/0169_MajorityElement.py
+++ leetcode/0169_MajorityElement.py
@@ -1,8 +1,34 @@
 from typing import List
 
+def majorityElement(nums: List[int]) -> int:
+        count = 0
+        candidate = 0
+        
+        for num in nums:
+            if count == 0:
+                candidate = num
+            
+            if num == candidate:
+                count += 1
+            else:
+                count -= 1
+        
+        return candidate
+
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
-        return sorted(nums)[len(nums) // 2]
+        # return sorted(nums)[len(nums) // 2]
+
+        count = {}
+        for x in nums:
+            if x in count:
+                count[x] += 1
+            else:
+                count[x] = 1
+        
+        for num in count:
+            if count[num] > len(nums) // 2:
+                return num
 
 if __name__ == "__main__":
     solution = Solution()
